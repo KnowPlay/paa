efd431a82c63021a8a028e4fa00ae242324dc587
Sarah <xiaowei.lin@outlook.com>
2025-03-19 22:13:40 +0000

    commit

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ from langchain_anthropic import ChatAnthropic
 # load environment variables from .env file
 load_dotenv()
 
-# set up LLM
+# set up llm 
+# llm = ChatOpenAI(model="gpt-4o-mini")
+llm2 = ChatAnthropic(model="claude-3-5-sonnet-20241022")
+
+# api key
+
